0cebc335cb7dab32298616b0802e21e03f56100c
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-04-07 19:34:15 -0400

    fix installation of lsl-v2

---
+++ lsl-v2/info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define name "lsl-v2")
+(define collection "lsl-v2")
+
+;; dependencies
+
+(define deps
+  '("errortrace-lib"
+    "base"
+    "lsl-v2-lib"))
+
+(define implies
+  '("lsl-v2-lib"))
