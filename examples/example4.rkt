03ae5a31bb38ff59a9526f5f41c2465d6d07e28e
Andrey Piterkin <andrey.piterkin@gmail.com>
2025-04-08 17:28:06 -0400

    uncomment example

--- examples/example4.rkt
+++ examples/example4.rkt
@@ -1,10 +1,10 @@
 #lang lsl-v2
 
 (define-contract (Leaf X) X)
-#;(define-contract (Tree X) (OneOf (Tuple X (Tree X) (Tree even?)) (Leaf X)))
+(define-contract (Tree X) (OneOf (Tuple X (Tree X) (Tree even?)) (Leaf X)))
 
-#;(define-contract NatTree (Tree natural?))
+(define-contract NatTree (Tree natural?))
 
-#;(: tree1 NatTree)
-#;(define tree1 (list 1 (list 2 3 4) 5))
+(: tree1 NatTree)
+(define tree1 (list 1 (list 2 3 4) 5))
 
